c588e1de8d4ded2ad49ce0b22455deff410fef52
giteel <gitlize@gmail.com>
2024-05-21 23:09:53 +0900

    올바른 괄호.java

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,21 +1,31 @@
+import java.util.Stack;
+
 class Solution {
     boolean solution(String s) {
-        boolean answer = false;
-        int count = 0;
-        for(int i = 0; i<s.length();i++){
-            if(s.charAt(i) == '('){
-                count++;
-            }
-            if(s.charAt(i) == ')'){
-                count--;
-            }
-            if(count < 0){
-                break;
+        boolean answer = true;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        //System.out.println(s);
+        
+        if ( s.charAt(0) != '(' || s.charAt(s.length()-1) !=')' ) return false;
+        
+        Stack<Character> stk = new Stack<>();
+        
+        for (int i=0; i<s.length(); i++) {
+            if ( s.charAt(i) == '(' ) {
+                stk.push(s.charAt(i));
+            } else if ( s.charAt(i) == ')' ) {
+                if (stk.isEmpty()) {
+                    answer = false;
+                    break;
+                } else {
+                    stk.pop();
+                }
             }
         }
-        if(count == 0){
-            answer = true;
-        }
+        
+        if (!stk.isEmpty()) answer = false;
+        
 
         return answer;
     }
