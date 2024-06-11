a9e3ad16471fa1bfbe8b1fa221392bf468fbe4b5
giteel <gitlize@gmail.com>
2024-06-11 14:11:26 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 0.05 ms, Memory: 68.4 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
@@ -1,20 +1,62 @@
+import java.util.List;
+import java.util.ArrayList;
 
-
-class Solution {
+public class Solution {
     public int solution(int n, long l, long r) {
         int answer = 0;
-        
-        for ( l--; l<r; l++) {
-            if (check(l)) answer++;
+        String kan = "11011";
+
+        List<Integer> tablel = new ArrayList<>();
+        List<Integer> tabler = new ArrayList<>();
+        l--; r--;
+        boolean fromOne = true;
+        boolean canl = true, canr = true;
+        long temp;
+        while (l != r) {
+            tablel.add((int) (l % 5));
+            tabler.add((int) (r % 5));
+            l /= 5;
+            r /= 5;
         }
-        
+
+        temp = l;
+        while (temp > 0) {
+            if (temp % 5 == 2) {
+                fromOne = false;
+                break;
+            }
+            temp /= 5;
+        }
+
+        if (!fromOne) return 0;
+        if (tablel.size() == 0) return 1;
+
+        for (int i = tablel.get(tablel.size() - 1) + 1; i < tabler.get(tabler.size() - 1); i++) {
+            if (kan.charAt(i) == '1') answer++;
+        }
+        if (tablel.get(tablel.size() - 1) == 2) canl = false;
+        if (tabler.get(tabler.size() - 1) == 2) canr = false;
+
+        for (int i = tablel.size() - 2; i >= 0; i--) {
+            answer *= 4;
+            if (canl) {
+                for (int j = tablel.get(i) + 1; j < 5; j++) {
+                    if (kan.charAt(j) == '1') answer++;
+                }
+                if (tablel.get(i) == 2) canl = false;
+            }
+
+            if (canr) {
+                for (int j = 0; j < tabler.get(i); j++) {
+                    if (kan.charAt(j) == '1') answer++;
+                }
+                if (tabler.get(i) == 2) canr = false;
+            }
+        }
+
+        if (canl && tablel.get(0) != 2) answer++;
+        if (canr && tabler.get(0) != 2) answer++;
+
         return answer;
-    }
-    
-    public boolean check(long l) {
-        if (l % 5 == 2) return false;
-        if (l < 5 ) return true;
-        
-        return ( check(l / 5));
     }
 }
