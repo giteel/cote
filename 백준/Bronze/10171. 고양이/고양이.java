2d191b2c303a6e89b34025c9c2e5538b23bdfab9
giteel <gitlize@gmail.com>
2024-06-16 11:01:37 +0900

    [Bronze V] Title: 고양이, Time: 72 ms, Memory: 11376 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("\\    /\\").append("\n");
+        sb.append(" )  ( ')").append("\n");
+        sb.append("(  /  )").append("\n");
+        sb.append(" \\(__)|");
+        
+        System.out.println(sb.toString());
+        
+    }
+}
