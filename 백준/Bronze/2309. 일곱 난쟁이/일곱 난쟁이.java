78509b428c9575233c1d356ddbffcdcf1f4c2db8
giteel <gitlize@gmail.com>
2024-06-16 18:57:58 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 76 ms, Memory: 11368 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -2,18 +2,19 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.StringTokenizer;
+//import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        //StringTokenizer st;
         int[] arr = new int[9];
         int total = 0;
         
         for(int i=0; i<9; i++) {
-            st = new StringTokenizer(br.readLine());
-            arr[i] = Integer.parseInt(st.nextToken());
+            //st = new StringTokenizer(br.readLine());
+            //arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(br.readLine());
             total += arr[i];
         }
         
