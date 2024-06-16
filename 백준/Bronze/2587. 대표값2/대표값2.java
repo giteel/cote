55ea79a7a4263b3622c2ca7e6e4eed4342c7e140
giteel <gitlize@gmail.com>
2024-06-16 17:12:11 +0900

    [Bronze II] Title: 대표값2, Time: 68 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.java
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -1,16 +1,20 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token;
         
         int sum = 0;
         int[] arr = new int[5];
         
         for (int i=0; i<5; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            token = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(token.nextToken());
             sum += arr[i];
         }
         
