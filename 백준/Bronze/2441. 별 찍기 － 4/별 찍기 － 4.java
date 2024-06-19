b4a048f556a65d79d589f60b23de3f2b99ebcbdc
giteel <gitlize@gmail.com>
2024-06-19 22:57:07 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 68 ms, Memory: 11692 KB -BaekjoonHub

--- 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -1,10 +1,13 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         br.close();
         StringBuilder sb= new StringBuilder();
@@ -18,6 +21,7 @@ public class Main {
             sb.append("\n");
         }
         
-        System.out.print(sb);
+        bw.write(sb.toString());
+        bw.flush();
     }
 }
